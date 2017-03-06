90d7e6f1ad2039917b0a523ce350d3ff12ebb797
AndreaInfUFSM <andrea@inf.ufsm.br>
2017-03-06 07:40:54 -0300

    Delete praticas, trabalhos. Update slides.

--- praticas/java1/OlaMundo.java
+++
@@ -1,9 +0,0 @@
-/** 
- * Esta classe implementa uma versao Java do programa que
- * imprime "Ola, mundo!" na saida padrao.
- */
-class OlaMundo {
-    public static void main(String[] args) {
-        System.out.println("Ola, mundo!"); // imprime mensagem
-    }
-}
